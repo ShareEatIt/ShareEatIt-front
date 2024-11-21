c9795e0664c471eae974357c6cd4949142adf20a
sj0919 <seungjinlee09@gmail.com>
2024-11-21 13:49:09 +0900

    💄 로그인 페이지 수정 및 UI 업데이트

--- src/pages/login/KakaoLoginPage.jsx
+++ src/pages/login/KakaoLoginPage.jsx
@@ -2,11 +2,15 @@ import { ReactComponent as Kakao } from "../../../src/assets/login/kakao.svg";
 import { ReactComponent as Logo } from "../../../src/assets/common/logo.svg";
 import { M, S } from "./login";
 
+export const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&prompt=login`;
 const LoginPage = () => {
+  console.log("login");
   return (
     <M.Layout>
       <Logo />
-      <Kakao />
+      <a href={KAKAO_AUTH_URI}>
+        <Kakao />
+      </a>
     </M.Layout>
   );
 };
