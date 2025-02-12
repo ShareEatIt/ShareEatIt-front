e0fa5676d3c89f93504e1c495ee9f8e7936b2a70
sj0919 <seungjinlee09@gmail.com>
2025-02-12 14:48:17 +0900

    💄intro 페이지 추가, 폰트 추가, UI 수정

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -8,12 +8,70 @@ const S = {
 };
 
 const M = {
+  //로그인 페이지지
   Layout: styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 150px;
-    padding: 150px;
+    gap: 5px;
+    padding: 100px;
+  `,
+  LogoWrapper: styled.div`
+    display: flex;
+    margin-bottom: 110px;
+  `,
+  //인트로 페이지
+  IntroLayout: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: var(--yellow-100);
+  `,
+  TextContainer: styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    position: relative;
+    margin-bottom: 270px;
+  `,
+  TextWrapper: styled.span`
+    font-size: 37px;
+    font-weight: 700;
+    margin-bottom: 20px;
+    padding-top: 80px;
+    line-height: 1.2;
+  `,
+  SubtextWrapper: styled.span`
+    font-size: 17px;
+    color: var(--white);
+    position: relative;
+    text-align: left;
+    left: -32px;
+    z-index: 2;
+    line-height: 1.3;
+  `,
+  IconOverlay: styled.div`
+    position: absolute;
+    left: 50px;
+    top: 10px;
+    z-index: 1;
+  `,
+  IntroLayout2: styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  `,
+  Title: styled.span`
+    font-size: 40px;
+    color: var(--yellow-100);
+  `,
+  Content: styled.span`
+    text-align: center;
+    color: var(--red);
   `,
 };
 
