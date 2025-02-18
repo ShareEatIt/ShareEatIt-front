f0e439fc494a54652b74b0d8d6221bd9b9d1037a
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2025-02-18 14:01:21 +0900

    💄 내비바 UI 변경

--- src/components/common/Navigition/navigationBar.jsx
+++ src/components/common/Navigition/navigationBar.jsx
@@ -1,32 +1,34 @@
 //화면 상단에 있는 뒤로가기 버튼과 상단에 글자 있을 경우 그것까지 만들기
 import { useNavigate } from "react-router-dom";
-import { S } from "./navigationBar.style";
+import {
+    BellWrapper,
+    ChatWrapper,
+    M,
+    MyPageWrapper,
+    S,
+} from "./navigationBar.style";
 
 const NavigationBar = ({ text }) => {
-    const navmenu = [
-        { name: "HOME", to: `/` },
-        { name: "CHAT", to: `/chatlist` },
-        { name: "NOTI", to: `/noti` },
-        { name: "MY", to: `/mypage` },
-    ];
-
-    const navigate = useNavigate();
+    const nav = useNavigate();
 
     return (
-        <S.Layout>
-            <S.LogoImg />
-            <S.NavContainer>
-                {navmenu.map((it) => (
-                    <S.NavLink
-                        key={it.name}
-                        to={it.to}
-                        activeclassname="active"
-                    >
-                        {it.name}
-                    </S.NavLink>
-                ))}
-            </S.NavContainer>
-        </S.Layout>
+        <M.Layout>
+            <M.LogoWrapper to="/">
+                <M.Logo />
+                <M.LogoText />
+            </M.LogoWrapper>
+            <M.NavWrapper>
+                <M.NavButton to="/chatlist" activeClassName="active">
+                    <ChatWrapper />
+                </M.NavButton>
+                <M.NavButton to="/noti" activeClassName="active">
+                    <BellWrapper />
+                </M.NavButton>
+                <M.NavButton to="/mypage" activeClassName="active">
+                    <MyPageWrapper />
+                </M.NavButton>
+            </M.NavWrapper>
+        </M.Layout>
     );
 };
 
