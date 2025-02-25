ca88482c1f1bbcd2a1cef48c1ad548246c1b84d8
gaeun0nueag <87455786+gaeun0nueag@users.noreply.github.com>
2025-02-25 14:12:42 +0900

    ♻️ 충돌 해결

--- src/components/common/Navigition/navigationBar.jsx
+++ src/components/common/Navigition/navigationBar.jsx
@@ -1,35 +1,31 @@
 import {
-  BellWrapper,
-  ChatWrapper,
-  M,
-  MyPageWrapper,
-  S,
+    BellWrapper,
+    ChatWrapper,
+    M,
+    MyPageWrapper,
+    S,
 } from "./navigationBar.style";
 
-
 const NavigationBar = ({ text }) => {
-  const nav = useNavigate();
-
-  return (
-    <M.Layout>
-      <M.LogoWrapper to="/home">
-        <M.Logo />
-        <M.LogoText />
-      </M.LogoWrapper>
-      <M.NavWrapper>
-        <M.NavButton to="/chatlist" activeClassName="active">
-          <ChatWrapper />
-        </M.NavButton>
-        <M.NavButton to="/notice" activeClassName="active">
-          <BellWrapper />
-        </M.NavButton>
-        <M.NavButton to="/mypage" activeClassName="active">
-          <MyPageWrapper />
-        </M.NavButton>
-      </M.NavWrapper>
-    </M.Layout>
-  );
-
+    return (
+        <M.Layout>
+            <M.LogoWrapper to="/home">
+                <M.Logo />
+                <M.LogoText />
+            </M.LogoWrapper>
+            <M.NavWrapper>
+                <M.NavButton to="/chatlist" activeClassName="active">
+                    <ChatWrapper />
+                </M.NavButton>
+                <M.NavButton to="/notice" activeClassName="active">
+                    <BellWrapper />
+                </M.NavButton>
+                <M.NavButton to="/mypage" activeClassName="active">
+                    <MyPageWrapper />
+                </M.NavButton>
+            </M.NavWrapper>
+        </M.Layout>
+    );
 };
 
 export default NavigationBar;
